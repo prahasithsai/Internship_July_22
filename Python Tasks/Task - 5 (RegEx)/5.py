22b894ba1862b0842082d90f4bde1fa1a3407fc7
prahasithsai <105191806+prahasithsai@users.noreply.github.com>
2022-08-07 22:27:54 +0530

    Add files via upload

---
+++ Python Tasks/Task - 5 (RegEx)/5.py
@@ -0,0 +1,11 @@
+# Re.start() & Re.end()
+S = input()
+k = input()
+import re
+pattern = re.compile(k)
+r = pattern.search(S)
+if not r:
+    print ("(-1, -1)")
+while r:
+    print ("({0}, {1})".format(r.start(), r.end() - 1))
+    r = pattern.search(S,r.start() + 1)
---
+++ Python Tasks/Task - 5 (RegEx)/6.py
@@ -0,0 +1,6 @@
+# Regex Substitution
+import re
+N = int(input())
+
+for i in range(N):
+    print (re.sub(r'(?<= )(&&|\|\|)(?= )', lambda x: 'and' if x.group() == '&&' else 'or', input()))
